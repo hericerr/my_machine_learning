d829c8875436e8e71a85b64cfb101a74bef6e072
hericerr <janhelcl@gmail.com>
2017-09-28 16:48:41 +0200

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -5,26 +5,23 @@ Created on Wed Sep 27 13:19:23 2017
 @author: Jan
 
 LogisticRegression
+LogisticLasso
 """
 import numpy as np
 from util import get_MNIST, test_models_classification
+import warnings
+warnings.filterwarnings("ignore")
 
 def sigmoid(z):
     return 1/(1 + np.exp(-z))
 
-def cross_entropy(T, Y):
-    E = 0
-    for i in xrange(len(T)):
-        if T[i] == 1:
-            E -= np.log(Y[i])
-        else:
-            E -= np.log(1 - Y[i])
-    return E
-
 class LogisticRegression(object):
     
+    def __init__(self, L2=0):
+        self.L2 = L2
+    
     def __str__(self):
-        return "LogisticRegression"
+        return "LogisticRegression(L2=%f)"%self.L2
         
     def fit(self, X, Y, learning_rate = 0.1, iters=100):
         N, D = X.shape
@@ -34,7 +31,7 @@ class LogisticRegression(object):
         for t in range(iters):
             Yhat = sigmoid(X0.dot(w))
             delta = Y - Yhat
-            w += learning_rate*(X0.T.dot(delta))
+            w += learning_rate*(X0.T.dot(delta) - self.L2*w)
         self.w = w
         
     def predict(self, X):
@@ -46,6 +43,25 @@ class LogisticRegression(object):
     def score(self, X, Y):
         P = np.round(self.predict(X))
         return np.mean(P == Y)
+    
+class LogisticLasso(LogisticRegression):
+    
+    def __init__(self, L1=0):
+        self.L1 = L1
+    
+    def __str__(self):
+        return "LogisticRegression(L1=%f)"%self.L1
+        
+    def fit(self, X, Y, learning_rate = 0.1, iters=100):
+        N, D = X.shape
+        X0 = np.ones((N, D+1))
+        X0[:, 1:] = X 
+        w = np.random.randn(D+1) / np.sqrt(D+1)       
+        for t in range(iters):
+            Yhat = sigmoid(X0.dot(w))
+            delta = Y - Yhat
+            w += learning_rate*(X0.T.dot(delta) + self.L1*np.sign(*w))
+        self.w = w    
         
         
 if __name__ == "__main__":
@@ -58,7 +74,5 @@ if __name__ == "__main__":
     Xtrain, Ytrain = X[:Ntrain], Y[:Ntrain]
     Xtest, Ytest = X[Ntrain:], Y[Ntrain:]
 
-    model = LogisticRegression()
+    model = LogisticRegression(L2=0.1)
     test_models_classification(Xtrain, Ytrain, Xtest, Ytest, [model])
-
-
